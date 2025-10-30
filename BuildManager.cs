d12a07d39b5516601881fe0a81e614aafc08768f
Yul-ljc <2129407743@qq.com>
2025-10-30 15:14:59 +0800

    Add files via upload

---
+++ BuildManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public TurretData StandardTurretData;
+    public TurretData MissileTurretData;
+    public TurretData LazerTurretData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
